a782d42da20ec7f539222e879b03ce4c402ae058
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-30 12:18:46 -0300

    finishing Test class

--- JavascriptTesting/01-aula/src/file.js
+++ JavascriptTesting/01-aula/src/file.js
@@ -8,13 +8,46 @@ const DEFAULT_OPTION = {
 };
 
 class File {
-  static async cvsToJson(filepPath) {
-    const content = await File.getFileContent(filepPath);
+  static async cvsToJson(filePath) {
+    const content = await File.getFileContent(filePath);
     const validation = File.isValid(content);
     if (!validation.valid) throw new Error(validation.error);
-
     return content;
+  }
+
+  static async getFileContent(filePath) {
+    return (await readFile(filePath)).toString("utf-8");
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [header, ...fileWithoutHeader] = csvString.split("\n");
+    const isHeaderValid = header === options.fields.join(",");
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    const isContentLengthAccepted =
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines;
+
+    if (!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    return { valid: true };
   }
 }
 
-module.export = File;
+(async () => {
+  const result = await File.cvsToJson("../mocks/threeItems-valid.csv");
+  //const result = await File.cvsToJson("./../mocks/invalid-header.csv");
+  console.log("result", result);
+})();
+
+module.exports = File;
