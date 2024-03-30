5c42f7af15240c500f2d100caa8738fb0f7bda9c
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-30 12:49:11 -0300

    feat: comments on methods

--- JavascriptTesting/01-aula/src/file.js
+++ JavascriptTesting/01-aula/src/file.js
@@ -9,6 +9,11 @@ const DEFAULT_OPTION = {
 
 class File {
   static async cvsToJson(filePath) {
+    // Método estático assíncrono que converte um arquivo CSV em JSON.
+    // Lê o conteúdo do arquivo usando getFileContent.
+    // Valida o conteúdo através de isValid.
+    // Se não for válido, lança um erro; caso contrário, retorna o conteúdo.
+
     const content = await File.getFileContent(filePath);
     const validation = File.isValid(content);
     if (!validation.valid) throw new Error(validation.error);
@@ -16,10 +21,17 @@ class File {
   }
 
   static async getFileContent(filePath) {
+    // Método estático assíncrono que lê o conteúdo de um arquivo.
+    // Usa readFile para ler o arquivo de forma assíncrona e converte o resultado para uma string UTF-8.
     return (await readFile(filePath)).toString("utf-8");
   }
 
   static isValid(csvString, options = DEFAULT_OPTION) {
+    //   Valida se o conteúdo do arquivo CSV atende às exigências especificadas em DEFAULT_OPTION.
+    //   Divide a string CSV por linhas e separa o cabeçalho do restante.
+    //   Verifica se o cabeçalho é válido comparando com os campos esperados.
+    //   Verifica se o número de linhas (excluindo o cabeçalho) está dentro do limite permitido.
+    //   Retorna um objeto indicando se o arquivo é válido e, se não for, inclui a mensagem de erro correspondente.
     const [header, ...fileWithoutHeader] = csvString.split("\n");
     const isHeaderValid = header === options.fields.join(",");
     if (!isHeaderValid) {
